deb52d41ebcbc381d06a878e70bad395de7e8865
surubhai441 <surubhai441@gmail.com>
2023-03-09 14:30:04 +0530

    hello

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
-console.log('how are you');
+// Hamburger js
+const burger = document.getElementById('_hamburger');
+const burgerLinks = document.getElementById('_hamburger__links');
+burger.addEventListener('click',()=>{
+    burger.classList.toggle('active');
+    burgerLinks.classList.toggle('active');
+})
+document.addEventListener('click',(event)=>{
+    // if(burger.classList.contains('active')||burgerLinks.classList.contains('active')){
+        const target = event.target;
+        if (!burger.contains(target)){
+            burger.classList.remove('active');
+            burgerLinks.classList.remove('active');
+        }
+    }
+)
 
